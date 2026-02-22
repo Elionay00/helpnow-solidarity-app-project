a42d29893be07e9bfe695fdcb2af478e359dbecc
MacBook Air <macbookair@Air-de-MacBook.local>
2026-02-22 15:09:51 -0300

    ajuste inicial app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.helpnow.app',
+  appName: 'ionic-app-base',
+  webDir: 'www'
+};
+
+export default config;
